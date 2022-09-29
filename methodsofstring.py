acaeda4ac0abcdc732158be451b9818ad7e5dabb
SRIVANI-GOWDA <srivaninmurthy@gmail.com>
2022-09-29 16:29:14 +0530

    python

---
+++ methodsofstring.py
@@ -0,0 +1,60 @@
+ # print("Hai wORld".lower())
+# print("Hai wORld".upper())
+# print("Hai wORld".swapcase())
+# my_message="hello world,hello universe"
+# print(my_message.count("""l"""))
+# print(my_message.count("hello",0,10)"""
+# sent = "today is a special day"
+# print(sent.count("day"))
+# s1 = "hello world"
+# print(s1.count('l',4,9))
+# my_message = "hello world"
+# print(my_message.find('o'))
+# print(my_message.rfind("o"))
+# print(my_message.find("o"))
+# print("today is a beautiful day".find("day"))
+# print("today is a beautiful day".rfind("day"))
+# my_message = "hello world"
+# print(my_message.index("g"))
+# print(my_message.index("o"))
+# print("malayalam".replace("#","@"))
+# print("how are you".startswith("a"))
+# print("how are you".endswith("a"))
+
+# lower() :converts a string to lower case
+# print("Hai WOrlD".lower())
+# print("###".lower())
+
+#upper() :converts a string to upper case
+# print("Hai WOrlD".upper())
+# print("6588".upper())
+
+# swapcase() : converts lower case into upper case and vice versa
+# print("Hai WOrlD".swapcase())
+# print("4556==-".upper())
+
+# count() :returns the occurence of substring in original string
+# print("Hai WOrlD".count("L"))
+# print("Hai WOrlD".count("L",10,12))
+
+
+# print("Hai WOrlD".rfind("L",1,2))
+# s6 = "*!#*$hai^%$*"
+# print(s6.strip("$"))
+
+a11 = "hai welcome to python"
+# print(a11.replace(" ",","))
+# print(a11.find("python"))
+# print(a11[::-1])
+# d ="sri"
+# e = "vani"
+# d = "hhhh"
+# print(d+e)
+# a = "hi"
+# print(a.replace("i","e"))
+# print(a)
+print(len(a11))
+
+
+
+
